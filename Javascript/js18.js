e945ea4194eb1c3576d8e66da8e17163ed11fce3
Diego Becco <d_becco_pereira@hotmail.com>
2022-08-12 19:41:08 -0300

    test 3

--- Javascript/js18.js
+++ Javascript/js18.js
@@ -1,33 +1,62 @@
-let value = 30
-let money = 50
+/*
+PRECONDICIÓN: 
+La máquina cuenta con 30 monedas de 10 y 50 monedas de 5.
+El precio de todos los productos es múltiplo de 5
+La máquina soporta solo billetes uruguayos (50, 100, 200, 500, 1.000, 2.000)
 
+Se ingresa el valor del producto (múltiplo de 5).
+Se ingresa el dinero entregado en la máquina.
+Se debe imprimir cuantas monedas de 10 y cuántas de 5 hay que entregar.
+En caso que el cambio no alcance se imprime “no hay cambio suficiente”
+*/
+
+//Givens
+let givenAProductCost = 30
+let givenHowMuchIPay = 50
+
+//When
+let change = calculateCoins(givenHowMuchIPay,givenAProductCost)
+
+// Expected
 let expectedCoinsOfTen = 2
 
-let coinsOfTen = 0
-let coinsOfFive = 0
+console.log(change.coinsOfTen == expectedCoinsOfTen)
 
-calculateCoins(money,value)
+//Given
+givenAProductCost = 35
+givenHowMuchIPay = 50
 
-console.log(coinsOfTen == expectedCoinsOfTen)
-
-value = 35
-money = 50
-coinsOfTen = 0
-coinsOfFive = 0
-
+//When
+change = calculateCoins(givenHowMuchIPay,givenAProductCost)
+//Expected
 expectedCoinsOfTen = 1
 let expectedCoinsOfFive = 1
 
-calculateCoins(money,value)
-
-console.log(coinsOfTen == expectedCoinsOfTen)
-console.log(coinsOfFive == expectedCoinsOfFive)
+console.log(change.coinsOfTen == expectedCoinsOfTen)
+console.log(change.coinsOfFive == expectedCoinsOfFive)
 
 
+// TEST -3 When the product cost 10 and i pay 320, receive 30 coins of 10 and 2 of 5
+//Given
+givenAProductCost = 10
+givenHowMuchIPay = 320
+
+change = calculateCoins(givenHowMuchIPay,givenAProductCost)
+
+//Expected
+expectedCoinsOfTen = 30
+expectedCoinsOfFive = 2
+//Assert
+console.log(change.coinsOfTen == expectedCoinsOfTen)
+console.log(change.coinsOfFive == expectedCoinsOfFive)
 
 function calculateCoins(money,value) {
-    let difference = money - value
-    coinsOfTen = Math.floor(difference / 10)
-    coinsOfFive = (difference%10)/5
+    let change = money - value
+    let coinsOfTen = Math.min(Math.floor(change / 10),30)
+    return {
+        coinsOfTen : coinsOfTen,
+        coinsOfFive : (change-(coinsOfTen*10))/5
+    }
+    
 }
 
